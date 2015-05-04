96e44fe2f3f07f50fd213285de109334bbba7313
root <root@master.inf.puppetlabs.demo>
2015-05-04 22:28:14 +0000

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class admin_users (
+  $userlist = [ 'temp' ]
+){
+
+  case $::osfamily {
+    'RedHat': {
+      user { $userlist:
+        ensure     => present,
+        password   => '$1$v4K9E8Wj$gZIHJ5JtQL5ZGZXeqSSsd0',
+        groups     => ['root'],
+        managehome => true,
+      }
+    }
+
+    'windows': {
+      user { $userlist:
+        ensure   => present,
+        password => 'puppet',
+        groups   => ['Administrators'],
+      }
+    }
+  }
+
+}
